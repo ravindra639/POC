784f8137f071bd660724d85edf5c3a07fe62a6a4
ravindra639 <42795993+ravindra639@users.noreply.github.com>
2024-08-07 20:46:07 +0530

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,22 +1,12 @@
 #!/bin/bash
-#file=$(ls | grep dev*)
-#echo $file
-
-# Update package lists
-sudo apt-get update
-
-# Install necessary prerequisites
-sudo apt-get install -y wget apt-transport-https
-
-# Download the Microsoft package repository GPG keys
-wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -o packages-microsoft-prod.deb
-
-# Register the Microsoft package repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb
 
 # Install the .NET SDK
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
+
+# Install ASP.NET Core Runtime
+sudo apt-get update
+sudo apt-get install -y aspnetcore-runtime-8.0
 
 # Verify installation
 dotnet --version
