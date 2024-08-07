7b77524a92218b3efdbcf4265191bc23f3eea6bc
ravindra639 <42795993+ravindra639@users.noreply.github.com>
2024-08-07 18:25:31 +0530

    Update install-dotnet.sh

--- install-dotnet.sh
+++ install-dotnet.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Update package lists
-#sudo apt-get update
-
-# Install necessary prerequisites
-#sudo apt-get install -y wget apt-transport-https
-
-# Download the Microsoft package repository GPG keys
-#wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-
-# Register the Microsoft package repository GPG keys
-# sudo dpkg -i packages-microsoft-prod.deb
-
 # Install the .NET SDK
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-8.0
